a79e3b802b9c0a6d2c2ec3fb857b41d6313fa2d7
demirbilek95 <dogancandemirbilek@gmail.com>
2019-10-19 20:43:28 +0200

    Solutions for exercises

--- exercises/c++/02_arrays_solutions/exercise_1.cc
+++ exercises/c++/02_arrays_solutions/exercise_1.cc
@@ -1,5 +1,22 @@
 #include <iostream>
-
 using namespace std;
+ 
+int main () {
+   int i, j;
+   int size = 0;
+   int primes[100];  // I prefer using one big array because it is easy to implement.
+   
+   for(i = 2; i<100; i++) {
+      for(j = 2; j <= (i/j); j++)
+         if(!(i%j)) break; // if factor found, not prime
+         if(j > (i/j)) {
+		primes[size] = i;
+		size++;
+		//cout << i << " is prime\n";
+		}
+   }
 
-int main()
+	for(auto v:primes) cout << v << endl;
+   
+   return 0;
+}
--- exercises/c++/02_arrays_solutions/exercise_2.cc
+++ exercises/c++/02_arrays_solutions/exercise_2.cc
@@ -1,26 +1,22 @@
 #include <iostream>
-
-int main(){
-	int primes[100];
-	int array_100[100];
-	for (std::size_t i{2}; i <= 100; ++i){
-    		array_100[i] = i;
-		//std::cout << array_100[i]<<'\n';
+using namespace std;
+ 
+int main () {
+   int i, j;
+   int size = 0;
+   int primes[100];  // I prefer using one big array because it is easy to implement.
+   
+   for(i = 2; i<100; i++) {
+      for(j = 2; j <= (i/j); j++)
+         if(!(i%j)) break; // if factor found, not prime
+         if(j > (i/j)) {
+		primes[size] = i;
+		size++;
+		//cout << i << " is prime\n";
 		}
-	
-	  for (std::size_t k{2}; k < 100; ++k){
-		//std::cout << array_100[k] << std::endl;
-		int n{2};
-		while(n<100){
-			
-			if(array_100[k] % n == 0)
-				break;
+   }
 
-			if(array_100[k] % n != 0)
-							
-			}		
-		}
-
-	return 0;
-		
+	for(auto v:primes) cout << v << endl;
+   
+   return 0;
 }
